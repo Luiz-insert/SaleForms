e388445ac2598ea05daa51a1b8987e2e14bb5d69
Luiz Gusttavo <lgusttavo29@gmail.com>
2024-12-15 23:50:59 -0300

    :rocket: Tentativa de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,10 +10,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <FormStep0 />
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FormStep0 />} />
+        {/* Adicione outras rotas aqui, se necessário */}
+      </Routes>
+    </Router>
   )
 }
     
